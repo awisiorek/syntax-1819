2750e897e6ef292ed47bc12ff1b506ab2d4cd868
aw <axel.wisiorek@lmu.de>
2018-10-22 16:51:29 +0200

    Übungsblatt 02, interaktive Aufgaben: update

--- uebung/exercises_2.py
+++ uebung/exercises_2.py
@@ -6,18 +6,18 @@ from questions import single, multiple, offen, selection, luecke
 
 
 blatt2_1a = [
-    [SingleChoice("Warum gibt die parse-Funktion des Skripts vermutlich eine Sequenz zurück?", "Ambiguität", "Reflexivität", "Dependenz", sonst=True, inst= single)],
-    [SingleChoice("Die Alternative <code>tree = parser.parse(sent)</code> ist weniger wünschenswert, da im Allgemeinen ________ Parse-Tree möglich ist.", "mehr als ein", "kein", "genau ein", sonst=True, inst=single)]
+    [SingleChoice("Warum gibt die parse-Funktion des Skripts vermutlich eine Sequenz zurück?", "strukturelle Ambiguität", "lexikalische Ambiguität", "Übergenerierung", sonst=True, inst= single)],
+    [SingleChoice("Die Alternative <code>tree = parser.parse(sent)</code> ist weniger wünschenswert, da im Allgemeinen ________ Parse-Tree möglich ist.", "mehr als ein", "genau ein", sonst=False, inst=single)]
     ]
 
 blatt2_1b = [
-    [MultipleChoice("Zu welcher Phrase kann die Präpositionalphrase \"in my pajamas\" gehören?", correct=["Nominalphrase", "Verbalphrase"],wrong=["Adverbialphrase","Präpositionalphrase"],sonst=False, inst=multiple )]
+    [MultipleChoice("Zu welcher Phrase kann die Präpositionalphrase \"in my pajamas\" gehören?", correct=["Objekt-Nominalphrase", "Verbalphrase"],wrong=["Adverbialphrase", "Attributphrase", "Subjekt-Nominalphrase"],sonst=False, inst=multiple )]
     ]
 
 blatt2_1c = [
+	    [SingleChoice("Bzgl. welcher (nicht an der Oberflächensyntax ersichtlichen) Eigenschaft unterscheiden sich die PPs in den beiden Analysen?", "syntaktische Funktion", "Wortart des Phrasenkopfes", "Phrasentyp", sonst=True, inst=single)],
     [SelectionList("Was ist der Fall, wenn die PP ein Adverbial ist?", "die im Verb ausgedrückte Tätigkeit wird modifiziert","die PP wird dem Substantiv beigefügt", "der Ort des Geschehens wird festgelegt", sonst=True, inst=selection)],
     [SelectionList("Was ist der Fall, wenn die PP ein Attribut ist?", "die PP wird dem Substantiv beigefügt", "der Ort des Geschehens wird festgelegt", "die im Verb ausgedrückte Tätigkeit wird modifiziert", sonst=True, inst=selection)]
-        #   ],"Der gegebene Satz ist ambig, da aus der Oberflächensyntax nicht ersichtlich ist, welche syntaktische Funktion die PP einnimmt.")
     ]
 
 
@@ -30,7 +30,7 @@ blatt2_2a = [
     ]
 
 blatt2_2b = [
-    [MultipleChoice("Welche Gesetzmäßigkeiten umfasst die Grammatik als Regelsystem?", correct=["phonologische", "morphologische", "syntaktische"],wrong=["semantische"],sonst=False, inst=multiple)],
+    [MultipleChoice("Welche Gesetzmäßigkeiten umfasst die Grammatik als Regelsystem?", correct=["phonologische", "morphologische", "syntaktische"],wrong=["semantische", "pragmatische"],sonst=False, inst=multiple)],
     [Lueckentext("Die Syntax ist ein Teilbereich der Grammatik, der sich auf die syntaktischen Regularitäten bezieht.", {"Teilbereich":["Oberbegriff","Synonym"], "syntaktischen":["morphologischen","phonologischen","semantischen"]},inst=luecke)]
     ]
 
@@ -38,12 +38,10 @@ blatt2_2b = [
 
 
 blatt2_3a = [
-#      [Anleitung("<br><h2>Aufgabe 3  &nbsp;&nbsp;&nbsp; Linguistische Strukturbeschreibung</h2>")],
-     [MultipleChoice("Welche Hierachie-Ebenen lassen sich in der Beschreibung des Aufbaus sprachlicher Strukturen unterscheiden?", correct=["Lautebene", "Wortebene", "Satzebene"],wrong=["Morphemebene","Phrasenebene"],sonst=False, inst=multiple)]
+     [MultipleChoice("Was sind die 3 grundlegenden Beschreibungsebenen der Grammatik natürlicher Sprachen?", correct=["Lautebene, Wortebene, Satzebene"],wrong=["Morphemebene, Phrasenebene, Satzebene", "Satzebene, Textebene, Bedeutungsebene"],sonst=False, inst=single)]
      ]
 
 blatt2_3b = [
-#     [Anleitung("<h4>Wo liegt der Unterschied zwischen dem Wortbegriff einer natürlichen Sprache und dem Wortbegriff einer formalen Sprache?<br> (Was sind die Blätter des Syntaxbaums einer Ableitung eines natürlichsprachlichen Satzes in einer die Sprache modellierenden formalen Grammatik?)</h4>")],
     [Lueckentext("Für die Blätter gilt: natürlichsprachliche Wörter sind eine Teilmenge aus dem Alphabet der Grundsymbole der formalen Sprache; ein analysierter natürlichsprachlicher Satz ist ein in der formalen Grammatik ableitbares Wort der formalen Sprache.", {"Wort":["Morphem","Symbol"], "Grundsymbole":["Nicht-Terminale"]},inst=luecke)]
     ]    
 
@@ -51,7 +49,7 @@ blatt2_3b = [
 
 
 blatt2_4a = [
-    [MultipleChoice("Welche formalen Repräsentationen der syntaktischen Struktur eines natürlichsprachlichen Satzes haben Sie in der Vorlesung kennengelernt?", correct=["Syntaxbaum", "Parsebaum", "Ableitungsbaum","Dependenzgraph"],wrong=[],sonst=True, inst=multiple)]
+    [MultipleChoice("Welche Typen von Syntaxbäumen (auch: Parsebäume, Ableitungsbäume) wurden gezeigt?", correct=["Konstituentenbaum mit Phrasenlabels als Knoten","Dependenzgraph mit gelabelten Kanten"],wrong=["Konstituentenbaum mit gelabelten Kanten", "Dependenzgraph mit Phrasenlabels als Knoten"],sonst=False, inst=multiple)]
     ]
 
 blatt2_4b = [
@@ -90,25 +88,30 @@ blatt2_4d_tree = """
 
 
 blatt2_5 = [
-    [SingleChoice("Was ist der Ursprung der Ambiguität?","Strukturelle Ambiguität","andere Gründe", "lexikalische Ambiguität", "morphologisch ambige Formen", sonst=False, inst=single)],
-    [MultipleChoice("In Bezug auf das Gerundium, welche Funktion können die Nomen haben?", correct=["Subjekt", "direktes Objekt"],wrong=["indirektes Objekt", "Argens", "Partiens"],sonst=False, inst=multiple)]    
+    [SingleChoice("Was ist der Ursprung der Ambiguität?","strukturelle Ambiguität", "lexikalische Ambiguität", "morphologisch ambige Formen", sonst=True, inst=single)],
+    [MultipleChoice("In welcher Beziehung können Gerund und nachfolgendes Nomen stehen?", correct=["Nomen ist Kopf einer NP-Phrase", "Gerund ist Kopf eines nichtfiniten Teilsatzes", "Nomen ist Dependent in einem nichtfiniten Teilsatz"],wrong=["Nomen ist Dependent einer NP-Phrase"],sonst=False, inst=multiple)],
+    [SingleChoice("Welche Funktion im Satz nimmt das Nomen ein, wenn es Kopf der Gerund-Nomen-Phrase ist?", "Subjekt", "Objekt", sonst=False, inst=single)],
+    [SingleChoice("Welche Funktion innerhalb der Gerund-Nomen-Phrase nimmt das Nomen ein, wenn das Gerund Kopf dieser Phrase ist?", "Objekt", "Subjekt", sonst=False, inst=single)],
+    [SingleChoice("Welche Funktion im Hauptsatz nimmt das Gerund ein, wenn es Kopf der Gerund-Nomen-Phrase ist?", "Subjekt", "Objekt", sonst=False, inst=single)],
+    [SingleChoice("Welche Funktion nimmt das Gerund ein, wenn das Nomen Kopf der Gerund-Nomen-Phrase ist?", "Attribut", "Objekt", sonst=False, inst=single)],
     ]
 
 
 
     
 blatt2_6a = [
-    [MultipleChoice("Hinsichtlich welcher syntaktischen Grundprinzipien werden natürliche Sprachen mit formalen Methoden analysiert?", correct=["Konstituenz", "Dependenz", "Phrasenstruktur","Abhängigkeitsstruktur"],wrong=["Wortstruktur"],sonst=True, inst=multiple)],
+    [MultipleChoice("Hinsichtlich welcher syntaktischen Grundprinzipien werden natürliche Sprachen mit formalen Methoden analysiert?", correct=["Konstituenz", "Dependenz"],wrong=["Synonymie", "Korrektheit"],sonst=False, inst=multiple)],
     [SingleChoice("Konstituenz ist synonym zu:","Phrasenstruktur","Dependenz", "Abhängigkeitsstruktur", "Wortstruktur", sonst=True, inst=single)],
     [SingleChoice("Dependenz ist synonym zu:","Abhängigkeitsstruktur", "Phrasenstruktur", "Konstituenz",  "Wortstruktur", sonst=True, inst=single)],
     ]
 
 blatt2_6b = [
-    [MultipleChoice("Mit welchen Mitteln werden natürliche Sprachen analysiert?", correct=["formale Grammatik", "Parser"],wrong=["Bäume"],sonst=True, inst=multiple)],
+    [SingleChoice("Welche Mittel kommen in einer formalen Syntaxanalyse zum Einsatz?", "formale Grammatik + Parser", "Parsebäume", "formale Grammatik", "Parser", sonst=False, inst=single)],
     ]
 
 blatt2_6c = [
-    [SingleChoice("Bei einer solchen Analyse wird welche ja/nein-Entscheidung unweigerlich getroffen?","Wohlgeformtheit", "Sinnhaftigkeit", "Korrektheit", sonst=True, inst=single)],
+    [MultipleChoice("Die ________ eines Satzes bzgl. der formalen Grammatik wird erkannt.", correct=["Wohlgeformtheit", "Grammatikalität"],wrong=["Sinnhaftigkeit"],sonst=False, inst=multiple)],
+#     [SingleChoice("Bei einer solchen Analyse wird welche ja/nein-Entscheidung unweigerlich getroffen?","Wohlgeformtheit", "Sinnhaftigkeit", "Korrektheit", sonst=True, inst=single)],
     ]
 
 blatt2_6d = [
