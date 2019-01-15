94d6d3a8ef30c47ff76ea0bb72f37a8832e3a886
mnschmit <mnschmit@users.noreply.github.com>
2019-01-15 16:02:29 +0100

    Übungsblatt 12 (Grammatikinduktion, Baumtransformationen und IE)

---
+++ uebung/exercises_12.py
@@ -0,0 +1,10 @@
+from questions import SingleChoice, OpenQuestion, SelectionList, Lueckentext, MultipleChoice
+from questions import single, multiple, offen, selection, luecke
+
+blatt12_5a = [
+    [MultipleChoice("Welche zwei Faktoren führen bei der syntaktischen Analyse natürlicher Sprache mittels formaler Grammatiken zu mehr Ambiguität (Anzahl an Ableitungen)?" , correct=["Zunahme der Anzahl von Regeln", "längere Sätze"], wrong=["kürzere Sätze", "Abnahme der Anzahl von Regeln"], inst="Wählen Sie die zwei korrekten Antworten aus.")]
+]
+
+blatt12_5b = [
+    [MultipleChoice("Welche zwei Arten von Ambiguität unterscheidet man hier?", correct=["strukturelle Ambiguität", "lexikalische Ambiguität"], wrong=["Attachment-Ambiguität", "Koordinierungsambigutiät"], inst="Wählen Sie die zwei korrekten Antworten aus.")]
+]
